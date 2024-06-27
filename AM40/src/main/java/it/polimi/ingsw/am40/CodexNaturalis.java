3af344d26351f572220837c499913ac5bc9ffd5c
Andrea <andrea5.croci@mail.polimi.it>
2024-06-27 19:47:27 +0200

    Fix CodexNaturalis main class for the handling of the cli args
    Signed-off-by: Andrea <andrea5.croci@mail.polimi.it>

--- AM40/src/main/java/it/polimi/ingsw/am40/CodexNaturalis.java
+++ AM40/src/main/java/it/polimi/ingsw/am40/CodexNaturalis.java
@@ -25,19 +25,11 @@ public class CodexNaturalis {
 
         switch (selection) {
             case 0 -> {;
-                String[] argsServer = new String[3];
-                argsServer[0] = args[0];
-                argsServer[1] = args[1];
-                argsServer[2] = args[2];
-                CodexNaturalisServer.main(argsServer);
+                CodexNaturalisServer.main(args);
             }
             case 1 -> {
-                String[] argsClient = new String[4];
-                argsClient[0] = args[0];
-                argsClient[1] = args[1];
-                argsClient[2] = args[2];
-                argsClient[3] = args[3];
-                CodexNaturalisClient.main(argsClient);
+
+                CodexNaturalisClient.main(args);
             }
             default -> {
                 System.err.println("Wrong selection. Exiting...");
