da6aaf59e58af179f81ec65166842038da7b8af6
Giovanni De Marco <giovanni.demarco@mail.polimi.it>
2024-06-28 15:41:11 +0200

    module-info corrected for compilation

--- AM40/src/main/java/module-info.java
+++ AM40/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module it.polimi.ingsw.am40 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.junit.jupiter.api;
     requires org.controlsfx.controls;
     requires json.simple;
     requires java.rmi;
